a3d343a45ad446fbeb9a5213aa4b10c8f4c8418e
Evan Cazares <schmoopy@Evans-Air.attlocal.net>
2022-05-10 11:40:35 -0500

    unique hotels working, just for the wrong column

--- functions.py
+++ functions.py
@@ -6,5 +6,33 @@
 import sys
 import pandas as pd
 
+# Global Tasks:
+# print unique hotels DONE
+# hotel with the highest average review
+# number of 5 star hotels
+# number of hotels with a tennis court
+#
+# Per Hotel Tasks:
+# average score
+# stars
+# number of rewviews
+# number of rooms
+
+def unique_hotels(csv_file):
+    # first created a list of all hotels with repeats
+    # that is the first column in the file
+    hotel_list = []
+    all_hotels = csv_file["User country"]
+    for index in all_hotels:
+        # print(index)
+        hotel_list.append(index)
+    # use a set to create a list of free of duplicates
+    unique_hotels = list(set(hotel_list))
+    return unique_hotels
+
+def best_hotel(csv_file):
+    # find out hotels and their scores
+    hotel_score = csv_file[["Name", "Score"]]
+    print(hotel_score)
 
 
--- main.py
+++ main.py
@@ -10,12 +10,21 @@ import functions as fn
 import pandas as pd
 
 def main():
-
-    # these two lines are all you need to run the program, but they do not do error
-    # checking to make sure the file exists before calling count_lines() and dispay_file()
-    # fix it to do proper error checking as shown in the examples. then erase this comment.
+    # .head() reads the first specified rows of data in the file
+    # varaible_name.iloc[] retunrs the infromation in a specifed row
+    # varaible_name.iloc[1,1] returns information in row index, column index1
+    # .tail() reads the last specified date in the file
+    # .columns() prints all the columns, they are also specfied in the csv file at the top
+    # variabel_name[''] or retunrs the specifc column, usuful for finding out all uniqe hotels in the file
+    # variabel_name[['Name', 'other column', 'other column']] will return more than one column, does not have to be in the same order as csv file
 
     all_data = pd.read_csv("hotel-reviews.csv")
-    print(all_data)
+    print("There are", len(fn.unique_hotels(all_data)), "unique hotels in this file.")
+    print("These hotels are: ")
+    for i in fn.unique_hotels(all_data):
+        print(i, ": ", end="")
+    print()
+    print(fn.best_hotel(all_data))
+
 
 main()
