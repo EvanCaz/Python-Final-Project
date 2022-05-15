c325ddfb06fe88d8fd3e715fbb9fdaf3419d44a4
Evan Cazares <schmoopy@Evans-Air.attlocal.net>
2022-05-15 01:17:50 -0500

    finishing touches

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import pandas as pd
 def main():
 
     all_data = pd.read_csv(sys.argv[1])
+    # Still need to error proof the command line pass through thingy
     print("There are", len(fn.unique_hotels(all_data)), "unique hotels in this file.")
     print(len(fn.tennis_star_hotels(all_data, True)), "of those hotels have five stars.")
     print(len(fn.tennis_star_hotels(all_data, False)), "of those hotels have tennis courts.")
